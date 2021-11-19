4c8ab6aa82c55e99180a68b245856fe2abec7b62
Yaroslav <ravlushevich.iarek@mail.ru>
2021-11-19 21:06:17 +0300

    project init

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+    
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
